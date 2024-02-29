59ad011e37d9546650c8f654a199936b3df6dccf
AntonioDam2B <antonioaguidam1@gmail.com>
2024-03-01 00:28:02 +0100

    Agregado abrir la galeria

--- ColorsPrueba/app/src/main/java/com/example/autocolorsprueba/galeria.kt
+++ ColorsPrueba/app/src/main/java/com/example/autocolorsprueba/galeria.kt
@@ -1,58 +1,187 @@
 package com.example.autocolorsprueba
 
 import android.app.Activity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.*
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.forEach
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.skydoves.colorpickerview.AlphaTileView
+import com.skydoves.colorpickerview.ColorPickerView
+import com.skydoves.colorpickerview.sliders.AlphaSlideBar
+import com.skydoves.colorpickerview.sliders.BrightnessSlideBar
+import kotlin.math.log
 
 class galeria : AppCompatActivity() {
-    private lateinit var botonImagen: Button
-    private lateinit var uri: Uri
+    lateinit var bottomNavigationView: BottomNavigationView
+
+    private lateinit var colorPickerView: ColorPickerView
+    private lateinit var alphaSlider: AlphaSlideBar
+    private lateinit var brightnessSlideBar: BrightnessSlideBar
+    private lateinit var alphaTileView: AlphaTileView
+    private lateinit var textView: TextView
+
+    // private lateinit var appToolbar: Toolbar
+    private lateinit var itemCopiar: MenuItem
+    private lateinit var itemAgregar: MenuItem
+    private lateinit var itemFav: MenuItem
+    private lateinit var itemComparar: MenuItem
+
+
+    private lateinit var toolbar: Toolbar
+
+    private var hexadecimal: String = ""
+
+
+    val pickMedia = registerForActivityResult(PickVisualMedia()){uri ->
+        if (uri != null){
+            Log.i("aris","Seleccionado")
+        }else{
+            Log.i("aris","No Seleccionado")
+        }
+    }
+
+    lateinit var btnImage: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_galeria)
 
-        botonImagen = findViewById(R.id.botonImagen)
+        alphaSlider = findViewById(R.id.alphaSlideBar)
+        brightnessSlideBar = findViewById(R.id.brightnessSlide)
+        alphaTileView = findViewById(R.id.alphaTileView)
+        textView = findViewById(R.id.textView)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        setupBottomMenu()
+        registerForContextMenu(alphaTileView)
+
+        btnImage = findViewById(R.id.botonImagen)
+        btnImage.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
+        }
+
     }
 
-    fun AbrirGaleria(activity: Activity, requestCode: Int) {
-////        // Crear un Intent para abrir la aplicación de la galería
-//        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-//
-//        // Verificar si hay alguna aplicación que pueda manejar la acción de abrir la galería
-//        if (intent.resolveActivity(activity.packageManager) != null) {
-//            // Iniciar la actividad de la galería esperando un resultado
-//            activity.startActivityForResult(intent, requestCode)
-//        }
-//        val intent = Intent()
-//        intent.action = android.content.Intent.ACTION_VIEW
-//        intent.type = "image/*"
-//        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-//        startActivity(intent)
-
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.setType("image/*")
-        galeriaArl.launch(intent)
+    private fun onItemSelectedListener(item: MenuItem): Boolean {
+        val itemId = item.itemId
+        when (item.itemId) {
+            R.id.selector -> {
+                val intent = Intent(this, MainActivity::class.java).apply {
+                }
+                startActivity(intent)
+            }
+//            R.id. page_fav -> {
+//                showPageFragment(R.drawable. ic_fav, R.string. bottom_nav_fav)
+//                return true
+//            }
+        }
+        return true
     }
 
-    val galeriaArl = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val data: Intent? = result.data
-            uri = data?.data
-            imagen.setImageURI(uri)
+    private fun setupBottomMenu() {
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setSelectedItemId(R.id.selector)
+
+        bottomNavigationView.setOnItemSelectedListener { item -> onItemSelectedListener(item) }
+
+        bottomNavigationView.menu.forEach { menuItem ->
+            val badge = bottomNavigationView.getOrCreateBadge(menuItem.itemId)
+            badge.isVisible = false
+            badge.badgeGravity = BadgeDrawable.TOP_START
+//            badgeCounts[menuItem.itemId] = 0
         }
     }
 
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.fav_menu, menu)
+        itemCopiar = menu.findItem(R.id.agregarFavoritos)
+        itemAgregar = menu.findItem(R.id.itemcopiar)
+        itemComparar = menu.findItem(R.id.comparar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        itemFav = menu.findItem(R.id.corazon)
+        // Asignar un click listener al ítem de menú corazón
+        itemFav.setOnMenuItemClickListener {
+            // Crear un Intent para iniciar la actividad de Galería
+            val intent = Intent(this@galeria, MainActivity::class.java)
+            // Iniciar la actividad
+            startActivity(intent)
+            // Devolver true para indicar que el evento ha sido consumido
+            true
+        }
+
+        // Devolver true para mostrar el menú en la barra de acción.
+
+
+        return true
+    }
+
+    //    override fun onContextMenuClosed (menu: Menu) {
+//        if (onContextItemSelected(itemCopiar))
+//        Toast.makeText( this, "Menú cerrado" , Toast.LENGTH_SHORT).show()
+//    }
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.agregarFavoritos -> {
+                showToast("Agregado a Favoritos")
+                return true
+            }
+
+            R.id.itemcopiar -> {
+
+                val textoACopiar = hexadecimal.toString()// Reemplaza esto con tu texto
+                copyToClipboard(textoACopiar)
+                showToast("Texto copiado al portapapeles")
+                return true
+            }
+
+            R.id.comparar -> {
+                val intent = Intent(this@galeria, MainActivity::class.java).apply {
+                }
+                startActivity(intent)
+                return true
+            }
+
+            else -> return super.onContextItemSelected(item)
+        }
+    }
+
+    private fun copyToClipboard(text: String) {
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData = ClipData.newPlainText("Texto copiado", text)
+        clipboardManager.setPrimaryClip(clipData)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
