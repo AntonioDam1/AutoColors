ae41243f08a15038b8abb6bb6f2fc219718aec96
dedalo6 <aperezdmor@educacion.navarra.es>
2024-03-13 00:49:03 +0100

    Añadida alguna documentacion, entities, y cifrado

--- ColorsPrueba/app/src/main/java/com/example/autocolorsprueba/model/entity/ColorFav.kt
+++ ColorsPrueba/app/src/main/java/com/example/autocolorsprueba/model/entity/ColorFav.kt
@@ -5,6 +5,24 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
 
+/**
+ * Esta clase representa la entidad ColorFav en la base de datos.
+ * Contiene información sobre un color de coche marcado como favorito.
+ *
+ * @param uid El identificador único del color favorito del coche.
+ * @param anio El año del coche al que pertenece el color favorito.
+ * @param marca La marca del coche al que pertenece el color favorito.
+ * @param modelo El modelo del coche al que pertenece el color favorito.
+ * @param nombrePintura El nombre del color de pintura del coche favorito.
+ * @param codigo El código del color de pintura del coche favorito.
+ * @param catalogueURL La URL del catálogo del coche favorito.
+ * @param hexadecimal El valor hexadecimal del color de pintura del coche favorito.
+ * @param red El componente rojo del color de pintura del coche favorito.
+ * @param green El componente verde del color de pintura del coche favorito.
+ * @param blue El componente azul del color de pintura del coche favorito.
+ * @param colorsampleURL La URL de la muestra de color del coche favorito.
+ * @param matchPercentage El porcentaje de coincidencia del color de pintura del coche favorito, puede ser nulo.
+ */
 @Entity
 data class ColorFav(
     @PrimaryKey(autoGenerate = true) val uid: Long,
